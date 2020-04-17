869ba3265124bfdb69b00976b8b47c6bcb693163
Monica Turner <turnermon@gmail.com>
2020-04-17 09:11:21 +0200

    Fixed import issues

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ import setuptools
 setuptools.setup(
     name='datasources-etl',
     url="mapaction/datasources-etl",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
     setup_requires=[],
     tests_require=[],
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'extract_adm0_cod=src.extract.cod:get_adm0',
-            'extract_adm0_gadm=src.extract.gadm:get_adm0',
+            'extract_adm0_cod=extract.cod:get_adm0',
+            'extract_adm0_gadm=extract.gadm:get_adm0',
         ]
     }
 )
