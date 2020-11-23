f803a6ba9bff28cd6dd4afa27663c691bf480015
Leon Baruah <leon.s.baruah@gmail.com>
2020-11-23 23:13:28 +0000

    amended get_wfp_airport_link() since it is a bare url that doesn't need input

--- src/extract/wfp.py
+++ src/extract/wfp.py
@@ -7,22 +7,19 @@ logger = logging.getLogger(__name__)
 
 
 ###############################################################################
-def get_wfp_airport_link(iso3, base_uri=None):
+def get_wfp_airport_link():
     """
     Fake look up function to convert a ISO3 code to a ourairportapi.com
     link (in full pipeline, this would rely on some ISO3 based lookup)
     :param iso3: ISO 3 code for country
     :return: url for country airport data from wfp_airports
     """
-    if base_uri is None:
-        base_uri = "https://geonode.wfp.org/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Awld_trs_airports_wfp&outputFormat=json&service=WFS&request=GetFeature"
+    base_uri = "https://geonode.wfp.org/geoserver/wfs" \
+               + "srsName=EPSG%3A4326&" \
+               + "typename=geonode%3Awld_trs_airports_wfp&" \
+               + "outputFormat=json&service=WFS&request=GetFeature"
 
-    if iso3 == 'YEM':
-        link_uri = base_uri + "Yemen"
-    else:
-        raise IOError("No country found with ISO3 code: %s" % iso3)
-
-    return link_uri
+    return base_uri
 
 
 ###############################################################################
@@ -31,19 +28,18 @@ def get_wfp_airports(output_airport_uri, iso3, wfp_airports_url):
     fetched our airports data from standard url
     :param output_airport_uri:
     :param iso3:
-    :param raw_data_uri:
+    :param wfp_airports_url:
     :return:
     """
 
-    # next_link = "https://geonode.wfp.org/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Awld_trs_airports_wfp&outputFormat=json&service=WFS&request=GetFeature"
-    next_link = get_wfp_airport_link(iso3, base_uri=wfp_airports_url)
-    iso3 = 'YEM'
+    if wfp_airports_url is None:
+        wfp_airports_url = get_wfp_airport_link()
 
     # Loop through pages and pull data from the API
     session = requests.Session()
 
     # print(next_link)
-    request = session.get(url=next_link)
+    request = session.get(url=wfp_airports_url)
     json = request.json()
 
     # Loop through flows and append data to dataframe
@@ -53,11 +49,11 @@ def get_wfp_airports(output_airport_uri, iso3, wfp_airports_url):
         airport_list.append(airport["properties"])
 
     airport_df = pd.DataFrame(airport_list)
+    apt_country = airport_df[airport_df['iso3'] == iso3]
 
-    apt_yem = airport_df[airport_df['iso3'] == iso3]
-
-    airport_pt = gpd.GeoDataFrame(apt_yem,
-        geometry=gpd.points_from_xy(apt_yem.longitude, apt_yem.latitude))
+    airport_pt = gpd.GeoDataFrame(apt_country,
+        geometry=gpd.points_from_xy(apt_country.longitude,
+                                    apt_country.latitude))
 
     # Set CRS
     if airport_pt.crs is None:
